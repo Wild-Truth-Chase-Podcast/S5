1a7e0ebb0a0eb6b0b9acb4b4e3200879dc89627c
nss350 <nss350@gmail.com>
2023-03-26 12:55:14 +0100

    Adding saving of the data locally to avoid calling the API on each run

--- fdic_bank_failures.py
+++ fdic_bank_failures.py
@@ -1,26 +1,46 @@
+"""
+An example of using the FDIC API failure endpoint
+"""
+from loguru import logger
+from pathlib import Path
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-fromyr = 1900
-toyr = 2023
-fields = "NAME,CERT,FIN,CITYST,FAILDATE,SAVR,RESTYPE,RESTYPE1,QBFDEP,QBFASSET,COST"
-totals = "QBFDEP,QBFASSET,COST"
-agg_sum_fields = "QBFASSET,QBFDEP,COST"
+def get_data():
+    """Get failure data"""
+    fromyr = 1900
+    toyr = 2023
+    fields = "NAME,CERT,FIN,CITYST,FAILYR,FAILDATE,SAVR,RESTYPE,RESTYPE1,QBFDEP,QBFASSET,COST"
+    limit = 10_000
+    totals = "QBFDEP,QBFASSET,COST"
+    agg_sum_fields = "QBFASSET,QBFDEP,COST"
 
-root = "https://banks.data.fdic.gov/api/failures"
-url = f'{root}?filters=FAILYR:["{fromyr}" TO "{toyr}"]&fields={fields}&sort_by=FAILDATE&sort_order=DESC&limit=2000&offset=0&total_fields={totals}'
-url += "&subtotal_by=RESTYPE"
-url += "&agg_by=FAILYR&agg_term_fields=RESTYPE&agg_sum_fields={agg_sum_fields}&agg_limit=120"
-print(url)
+    root = "https://banks.data.fdic.gov/api/failures"
+    url = f'{root}?filters=FAILYR:["{fromyr}" TO "{toyr}"]&fields={fields}'
+    url += '&sort_by=FAILDATE&sort_order=DESC'
+    url += f'&limit={limit}&offset=0'
+    # url += '&total_fields={totals}&subtotal_by=RESTYPE'
+    # url += '&agg_by=FAILYR&agg_term_fields=RESTYPE&agg_sum_fields={agg_sum_fields}&agg_limit=120'
+    logger.info(url)
 
-response = requests.get(url)
-results = response.json()
-results = [x["data"] for x in results["data"]]
-df = pd.DataFrame.from_records(results)
-df = df.sort_values("FAILYR")
+    response = requests.get(url)
+    results = response.json()
+    results = [x["data"] for x in results["data"]]
+    df = pd.DataFrame.from_records(results)
+    return df.sort_values("FAILYR")
+
+csv_path = Path("FDIC", "data", "failures.csv")
+overwrite = False
+if overwrite or not csv_path.exists():
+    logger.info("No existing data found, calling API")
+    df = get_data()
+    df.to_csv(csv_path)
+else:
+    logger.info("Data found on disk, reading data")
+    df = pd.read_csv(csv_path)
+
 print(df)
-
-plt.plot(df["FAILYR"], df["count"])
-plt.xticks(rotation = 45)
-plt.show()
+# plt.plot(df["FAILYR"], df["count"])
+# plt.xticks(rotation = 45)
+# plt.show()
--- fdic_financial_reports.py
+++ fdic_financial_reports.py
@@ -1,25 +1,45 @@
+"""
+An example of using the FDIC API financials endpoint
+"""
+from loguru import logger
+from pathlib import Path
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-bank = "SILICON VALLEY BANK"
-fields = "NAME,STNAME,REPYEAR,RISDATE,REPDTE,ASSET,LIAB"
-limit = 200
 
-root = 'https://banks.data.fdic.gov/api/financials'
-url = f'{root}?filters=NAME:"{bank}"&fields={fields}&limit={limit}'
+def get_data():
+    """Get financials data"""
+    bank = "SILICON VALLEY BANK"
+    fields = "NAME,STNAME,REPYEAR,RISDATE,REPDTE,ASSET,LIAB"
+    limit = 200
+
+    root = 'https://banks.data.fdic.gov/api/financials'
+    url = f'{root}?filters=NAME:"{bank}"&fields={fields}&limit={limit}'
+    logger.info(url)
+
+    response = requests.get(url)
+    results = response.json()
+    results = [x["data"] for x in results["data"]]
+    df = pd.DataFrame.from_records(results)
+    df["REPDTE"] = pd.to_datetime(df["REPDTE"])
+    df["RISDATE"] = pd.to_datetime(df["RISDATE"])
+    df["REPYEAR"] = pd.to_datetime(df["REPYEAR"])
+    df["DIFF"] = df["ASSET"] - df["LIAB"]
+    return df
+
+csv_path = Path("FDIC", "data", "financials.csv")
+overwrite = False
+if overwrite or not csv_path.exists():
+    logger.info("No existing data found, calling API")
+    df = get_data()
+    df.to_csv(csv_path)
+else:
+    logger.info("Data found on disk, reading data")
+    df = pd.read_csv(csv_path)
 
 
-response = requests.get(url)
-results = response.json()
-results = [x["data"] for x in results["data"]]
-df = pd.DataFrame.from_records(results)
-df["REPDTE"] = pd.to_datetime(df["REPDTE"])
-df["RISDATE"] = pd.to_datetime(df["RISDATE"])
-df["REPYEAR"] = pd.to_datetime(df["REPYEAR"])
-df["DIFF"] = df["ASSET"] - df["LIAB"]
 print(df)
-
 plt.figure()
 plt.semilogy(df["REPDTE"], df["ASSET"], label="total assets")
 plt.semilogy(df["REPDTE"], df["LIAB"], label="total liabilities")
