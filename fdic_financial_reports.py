b562668ede37f157000aca7eef6e44cad8c97676
nss350 <nss350@gmail.com>
2023-03-26 16:08:02 +0100

    Switching to using plotly

--- fdic_financial_reports.py
+++ fdic_financial_reports.py
@@ -5,16 +5,23 @@ from loguru import logger
 from pathlib import Path
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 def get_data():
     """Get financials data"""
     bank = "SILICON VALLEY BANK"
-    fields = "NAME,STNAME,REPYEAR,RISDATE,REPDTE,ASSET,LIAB"
+    fields = "NAME,STNAME,REPYEAR,RISDATE,REPDTE"
+    # add fields about assets
+    fields += ",ASSET"
+    # add fields about liabilities
+    fields += ",DEP,LIAB"
+    # add any other fields
+    fields += ""
     limit = 200
 
-    root = 'https://banks.data.fdic.gov/api/financials'
+    root = "https://banks.data.fdic.gov/api/financials"
     url = f'{root}?filters=NAME:"{bank}"&fields={fields}&limit={limit}'
     logger.info(url)
 
@@ -28,6 +35,7 @@ def get_data():
     df["DIFF"] = df["ASSET"] - df["LIAB"]
     return df
 
+
 csv_path = Path("FDIC", "data", "financials.csv")
 overwrite = False
 if overwrite or not csv_path.exists():
@@ -40,18 +48,37 @@ else:
 
 
 print(df)
-plt.figure()
-plt.semilogy(df["REPDTE"], df["ASSET"], label="total assets")
-plt.semilogy(df["REPDTE"], df["LIAB"], label="total liabilities")
-plt.legend(loc="upper left")
-plt.twinx()
-plt.bar(df["REPDTE"], df["DIFF"], label="Assets - Liabilities")
-plt.gca().set_yscale("log")
-plt.legend(loc="upper right")
-
-
-plt.figure()
-# plt.bar(df.index, df["DIFF"])
-plt.bar(df.index, (df["DIFF"] / df["ASSET"]) * 100)
-
-plt.show()
+fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
+fig.add_trace(
+    go.Scatter(
+        x=df["REPDTE"],
+        y=df["ASSET"],
+        mode="lines+markers",
+        name="Total assets",
+    ),
+    row=1,
+    col=1,
+)
+fig.add_trace(
+    go.Scatter(
+        x=df["REPDTE"],
+        y=df["LIAB"],
+        mode="lines+markers",
+        name="Total liabilities",
+    ),
+    row=1,
+    col=1,
+)
+fig.add_trace(
+    go.Bar(
+        x=df["REPDTE"],
+        y=(df["LIAB"] / df["ASSET"]) * 100,
+        name="Liabilities/Assets",
+    ),
+    row=2,
+    col=1,
+)
+fig.update_layout(
+    yaxis=dict(title="Dollars $", type="log"), yaxis2=dict(title="Liabilities/Assets %")
+)
+fig.show()
